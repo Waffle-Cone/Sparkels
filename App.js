2967911ac485feec5b267077a8709660275fdacf
Camilla Bergs <camillabergantinos20@yahoo.com>
2024-02-09 16:37:03 +0000

    First Branch Commit

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Sparkles</Text>
+      <Text>Sparkles</Text>
       <StatusBar style="auto" />
     </View>
   );

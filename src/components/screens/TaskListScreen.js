6ffaa7c98dc410f85df668ea0ee9a28bbfb8dff4
Camilla Bergs <camillabergantinos20@yahoo.com>
2024-02-28 22:06:45 +0000

    Added handleDelete function to delete a project

--- src/components/screens/TaskListScreen.js
+++ src/components/screens/TaskListScreen.js
@@ -4,21 +4,42 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Alert,
 } from "react-native";
 import React, { useContext, useEffect, useState } from "react";
 import { ProjectContext } from "../context/ProjectContext";
 import Icons from "../UI/Icons";
 
-const TaskListScreen = () => {
+const TaskListScreen = ({ navigation, route }) => {
   // Initialisations ------------------
-
+  const { projectId, projectName, projectDescription, projectDueDate } =
+    route.params;
+  const { handleDelete } = useContext(ProjectContext);
   // State ---------------------------
   // Handlers -------------------------
+  const onDelete = () => {
+    handleDelete(projectId);
+    navigation.goBack();
+  };
+
+  const requestDelete = () =>
+    Alert.alert(
+      "Delete warning",
+      `Are you sure that you want to delete Project ${projectName}`,
+      [{ text: "Cancel" }, { text: "Delete", onPress: onDelete }]
+    );
   // View -----------------------------
 
   return (
     <View style={styles.container}>
-      <Text style={styles.h1}>Your Tasks</Text>
+      <Text style={styles.h1}>Your Tasks for Project {projectName}</Text>
+      <Text style={styles.h2}>Description: {projectDescription}</Text>
+      <Text style={styles.h2}>Due Date: {projectDueDate}</Text>
+      <View style={styles.buttonTray}>
+        <TouchableOpacity style={styles.deleteButton} onPress={requestDelete}>
+          <Text style={styles.textDeleteButton}>Delete</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -31,11 +52,43 @@ const styles = StyleSheet.create({
     margin: 20,
   },
   h1: {
+    paddingBottom: 10,
     alignItems: "center",
     justifyContent: "center",
-    paddingTop: 5,
     fontSize: 24,
     fontWeight: "bold",
     color: "black",
+  },
+  h2: {
+    paddingBottom: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 16,
+    color: "black",
+  },
+  buttonTray: {
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  deleteButton: {
+    flexDirection: "row",
+    height: 50,
+    width: 100,
+    borderRadius: 10,
+    borderTopWidth: 2,
+    borderLeftWidth: 2,
+    borderRightWidth: 2,
+    borderBottomWidth: 4,
+    borderColor: "black",
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textDeleteButton: {
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#24325B",
+    paddingVertical: 8,
   },
 });
